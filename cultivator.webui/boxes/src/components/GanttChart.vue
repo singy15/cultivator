61d46d46b3e0a392d294819c5d9bc257ad040d40
kedama <uesastorage3@gmail.com>
2024-10-23 07:33:37 +0900

    impl

--- cultivator.webui/boxes/src/components/GanttChart.vue
+++ cultivator.webui/boxes/src/components/GanttChart.vue
@@ -1,5 +1,6 @@
 <script>
 import "../boxes.css";
+import moment from "moment";
 
 export default {
   components: {
@@ -7,25 +8,50 @@ export default {
   data() {
     return {
       calendar: [
-        { date: "1/1" },
-        { date: "1/2" },
-        { date: "1/3" },
-        { date: "1/4" },
-        { date: "1/5" },
+        { date: moment().add(1,"days").toDate() },
+        { date: moment().add(2,"days").toDate() },
+        { date: moment().add(3,"days").toDate() },
+        { date: moment().add(4,"days").toDate() },
+        { date: moment().add(5,"days").toDate() },
       ],
       rows: [
-        { subject: "test1", },
-        { subject: "test2", },
-        { subject: "test3", },
-        { subject: "test3", },
-        { subject: "test3", },
-        { subject: "test3", },
-        { subject: "test3", },
+        { id: "t1", subject: "task1", },
+        { id: "t2", subject: "task2", },
+        { id: "t3", subject: "task3", },
+      ],
+      costPl: [
+        { taskId: "t1", date: moment().add(2,"days").toDate(), cost: 2.0 },
+        { taskId: "t1", date: moment().add(3,"days").toDate(), cost: 2.0 },
+        { taskId: "t1", date: moment().add(5,"days").toDate(), cost: 1.0 },
+        { taskId: "t2", date: moment().add(2,"days").toDate(), cost: 2.0 },
+      ],
+      costAc: [
+        { taskId: "t1", date: moment().add(2,"days").toDate(), cost: 2.0 },
+        { taskId: "t1", date: moment().add(3,"days").toDate(), cost: 2.0 },
+        { taskId: "t1", date: moment().add(5,"days").toDate(), cost: 1.0 },
+        { taskId: "t2", date: moment().add(2,"days").toDate(), cost: 2.0 },
       ]
     }
   },
   watch: { },
+  computed: {
+    costInput: function() {
+      return this.rows.map(r => {
+        let costsPl = new Array(this.calendar.length);
+        let costsAc = new Array(this.calendar.length);
+
+        this.calendar.forEach(c)
+
+        this.costPl.filter(p => p.taskId === r.id 
+
+        return [costsPl, costsAc];
+      });
+    }
+  },
   methods: {
+    formatDateYM(date) {
+      return moment(date).format("MM/DD");
+    }
   },
   mounted() {
   }
@@ -44,25 +70,30 @@ export default {
     <div class="box" style="width:20em">&nbsp;</div>
     <div class="box-container">
       <template v-for="date in calendar">
-        <div class="box bt bl" :style="{width:`${4}em`, textAlign:`center`}">{{ date.date }}</div>
+        <div class="box bt bl" :style="{width:`${4}em`, justifyContent:`center`}">
+          {{ formatDateYM(date.date) }}
+        </div>
       </template>
     </div>
 
     <br/>
 
     <template v-for="(row, i) in rows">
-      <!-- <div class="box-container bb-last"> -->
         <div class="box bt" style="width:20em;">
           <input />
         </div>
 
         <template v-for="date in calendar">
-          <div class="box bl bt" :style="{width:`${4}em`}">
-            <input />
+          <div class="box bl bt" :style="{width:`${4}em`, flexDirection:`column`}">
+            <input :style="{textAlign:`center`, fontSize:`0.8em`,
+              backgroundColor:`rgba(200,200,255,0.7)`
+              }" :value="0"/>
+            <input :style="{textAlign:`center`, fontSize:`0.8em`,
+              backgroundColor:`rgba(255,255,200,0.7)`
+              }" :value="0"/>
           </div>
         </template>
 
-      <!-- </div> -->
       <br v-if="i !== (rows.length - 1)" />
     </template>
 
@@ -114,7 +145,7 @@ export default {
     /*height: calc(100% - 1px);*/
     /*height: 1.5em;*/
     height: 100%;
-    line-height: 1.5em;
+    line-height: 1em;
     font-size: 1em;
     color: inherit;
     margin: 0;
